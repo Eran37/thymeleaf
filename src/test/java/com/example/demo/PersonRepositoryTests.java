2102152249c0bae1b9d0eba51abeb00a9230be8a
Eran37 <doc.assraf@gmail.com>
2020-09-21 19:03:34 +0300

    Updated

---
+++ src/test/java/com/example/demo/PersonRepositoryTests.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import com.example.demo.entities.Person;
+import com.example.demo.repository.PersonRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@SpringBootTest
+class PersonControllerTests {
+
+    @Autowired
+    private PersonRepository repository;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void createPerson() {
+        List<Person> persons = Stream.of(
+                new Person("Avi", "Cohen", 33),
+                new Person("Ariel", "David", 29),
+                new Person("Benny", "Segal", 54),
+                new Person("Eran", "Or", 15),
+                new Person("Bar", "Sharabani", 43),
+                new Person("Shir", "Magal", 27)
+        )
+                .map(repository::save)
+                .collect(Collectors.toList());
+
+        System.out.println(persons);
+    }
+
+}
